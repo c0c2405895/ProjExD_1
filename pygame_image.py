5ab7c0812a44bea8fed9eb12d2c814398a0ac43b
Renri <c0c2405895@edu.teu.ac.jp>
2025-06-10 16:49:09 +0900

    演習１の２

--- pygame_image.py
+++ pygame_image.py
@@ -18,20 +18,26 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300,200
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+        dx = 0
+        dy = 0
         key_list = pg.key.get_pressed()
         if key_list[pg.K_UP]:
-            kk_rct.move_ip(0,-1)
+            dx = -1
         if key_list[pg.K_DOWN]:
-            kk_rct.move_ip(0,1)
+            dx = 1
         if key_list[pg.K_LEFT]:
-            kk_rct.move_ip(-1,0)
+            dy = -1
         if key_list[pg.K_RIGHT]:
-            kk_rct.move_ip(1,0)
+            dy = 1
         else:
-            kk_rct.move_ip(-1,0)
+            dy = -1
+
+        kk_rct.move_ip(dy,dx)
 
         x = tmr%3200
         screen.blit(bg_img, [-x, 0]) #1枚目
